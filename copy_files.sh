581c2d833164757185a2a59f3d135ac24616d610
Devin Sze <devinsze11@gmail.com>
2024-10-08 01:36:06 -0700

    javascript fileshare

--- copy_files.sh
+++ copy_files.sh
@@ -2,5 +2,5 @@ for folder in "about" "portfolio"
 do
     cp style.css ${folder}/style.css        # copy css to 2 other primary folders
     cp logo_* ${folder}                     # copy logos to 2 other primary folders
-
+    cp home.js ${folder}/home.js            # copy js functions to 2 other primary folders
 done
