a56c0005b45ca01a455b721cc9b645a0e7d2430a
Devin Sze <devinsze11@gmail.com>
2024-10-08 00:02:23 -0700

    Website commit

---
+++ home.js
@@ -0,0 +1,327 @@
+// LOGO STUFF
+
+var DARK_MODE = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+// console.log(DARK_MODE)
+
+
+
+// var light_transparent_color_0 ="#8080805f";
+var is_hovering = false;
+var containerHoverOn = false;
+
+
+var unit = 17;
+
+
+
+
+var light_color_grey_1 = "#808080"; //ham hover color
+var light_color_grey_2 = "#b7b7b7"; //ham color
+
+var logo_white = "#808080"; //hover accent color
+var logo_color_3 = "#afafafc0"; //hover background color
+var logo_color_1 = light_color_grey_2; //"#b7b7b7"; //original logo color
+var transparent_color = "#ffffff00";
+
+var light_transparent_color_0 = "#8080805f";
+var light_transparent_color_6 = "#808080c4";
+var light_shadow_hover = "3px 3px 3px " + light_transparent_color_0;
+var light_shadow_active = "1.5px 1.5px 2px " + light_transparent_color_6;
+
+var dark_transparent_color_0 = "#232323c4";
+var dark_shadow_hover = "3px 3px 3px " + dark_transparent_color_0;
+
+
+
+var shadow_hover = light_shadow_hover;
+
+if (DARK_MODE) {
+    logo_white = "white";
+    light_color_grey_1 = "white";
+    shadow_hover = dark_shadow_hover; //light_shadow_hover;
+}
+
+
+let _ids = ["d1", "s1", "s3"]
+let _d = ["d1", "d3"]
+let _s = ["s1", "s2", "s3"]
+
+
+
+
+
+
+
+function logo_hover() {
+    d3 = document.getElementById("d3");
+    s2 = document.getElementById("s2");
+    is_hovering = !is_hovering;
+    if (is_hovering) {
+        
+        for (let i = 0; i < _ids.length; i++) {
+            let _id = _ids[i]
+            document.getElementById(_id).style.backgroundColor = logo_white;
+        }
+        document.getElementById("outer_container").style.borderColor = logo_white;
+
+        d3.style.backgroundColor = logo_color_3;
+        s2.style.backgroundColor = logo_color_3;
+        document.getElementById("outer_container").style.borderColor = transparent_color;
+        for (let i = 0; i < _d.length; i++) {
+            document.getElementById(_d[i]).style.transform = "translateX(-"+unit/8+"px)"
+        }
+        for (let i = 0; i < _s.length; i++) {
+            document.getElementById(_s[i]).style.transform = "translateX("+unit/8+"px)"
+        }
+
+
+    } else {
+        for (let i = 0; i < _ids.length; i++) {
+            let _id = _ids[i]
+            document.getElementById(_id).style.backgroundColor = logo_color_1;
+        }
+        document.getElementById("outer_container").style.borderColor = logo_color_1;
+
+
+        d3.style.backgroundColor = transparent_color;
+        s2.style.backgroundColor = transparent_color;
+
+        document.getElementById("outer_container").style.borderColor = logo_color_1;
+        for (let i = 0; i < _d.length; i++) {
+            document.getElementById(_d[i]).style.transform = "translateX(0px)"
+        }
+        for (let i = 0; i < _s.length; i++) {
+            document.getElementById(_s[i]).style.transform = "translateX(0px)"
+        }
+    }
+}
+
+
+function arrowLeft(x) {
+    x.children[0].style.transform = "skewX(-12deg)"
+    x.children[1].style.transform = "translateX(6px) scale(0.9, 1)"
+}
+function arrowReset(x) {
+    x.children[0].style.transform = "skewX(0deg)"
+    x.children[1].style.transform = "translateX(0)"
+}
+
+var hamburger_height = 22;
+var ham_size = 4.5;
+var ham_gap = (hamburger_height - 3*ham_size)/2;
+var ham1_btm = String(ham_size) + "px";
+var ham2_top = String(ham_size + ham_gap) + "px";
+var ham2_btm = String(2*ham_size + ham_gap) + "px";
+var ham3_top = String(2*ham_size + 2*ham_gap) + "px";
+
+var ham_size_hover = 5.2;
+var ham_gap_hover = (hamburger_height - 3*ham_size_hover)/2;
+var ham1_btm_hover = String(ham_size_hover) + "px";
+var ham2_top_hover = String(ham_size_hover + ham_gap_hover) + "px";
+var ham2_btm_hover = String(2*ham_size_hover + ham_gap_hover) + "px";
+var ham3_top_hover = String(2*ham_size_hover + 2*ham_gap_hover) + "px";
+
+var x_size = 3;
+var x_top = String(x_size) + "px";
+var x_btm = String(hamburger_height - x_size) + "px";
+
+var x_size_hover = 4;
+var x_top_hover = String(x_size_hover) + "px";
+var x_btm_hover = String(hamburger_height - x_size_hover) + "px";
+
+
+var displayed = "ham";
+
+function manageHam() {
+    if (displayed == "ham") {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+        document.getElementById("ham1").style.clipPath = "polygon(0% "+x_top+", 0% 0%, "+x_top+" 0%, 100% "+x_btm+", 100% 100%, "+x_btm+" 100%)";
+        document.getElementById("ham2").style.clipPath = "polygon(50% "+ham2_top+", 50% "+ham2_top+", 50% "+ham2_btm+", 50% "+ham2_btm+")";
+        document.getElementById("ham3").style.clipPath = "polygon(0% "+x_btm+", "+x_btm+" 0%, 100% 0%, 100% "+x_top+", "+x_top+" 100%, 0% 100%)";
+
+        document.getElementById("nav_container_small").style.top = "50px";
+        document.getElementById("buffer").style.height = "200px";
+
+        displayed = "x";
+    } else {
+        document.getElementById("ham1").style.clipPath = "polygon(0% "+ham1_btm+",0% 0%, "+ham1_btm+" 0%, 100% 0%, 100% "+ham1_btm+", 100% "+ham1_btm+")";
+        document.getElementById("ham2").style.clipPath = "polygon(0% "+ham2_top+", 100% "+ham2_top+", 100% "+ham2_btm+", 0% "+ham2_btm+")";
+        document.getElementById("ham3").style.clipPath = "polygon(0% "+ham3_top+", "+ham3_top+" "+ham3_top+", 100% "+ham3_top+", 100% 100%, "+ham1_btm+" 100%, 0% 100%)";
+        
+        document.getElementById("nav_container_small").style.top = "-150px";
+        document.getElementById("buffer").style.height = "0px";
+
+
+        displayed = "ham";
+    }
+}
+
+
+
+function hoverHam(action) {
+    if (displayed == "ham") {
+        if (action == 'onmouseover') {
+            document.getElementById("ham1").style.clipPath = "polygon(0% "+ham1_btm_hover+",0% 0%, "+ham1_btm_hover+" 0%, 100% 0%, 100% "+ham1_btm_hover+", 100% "+ham1_btm_hover+")";
+            document.getElementById("ham2").style.clipPath = "polygon(0% "+ham2_top_hover+", 100% "+ham2_top_hover+", 100% "+ham2_btm_hover+", 0% "+ham2_btm_hover+")";
+            document.getElementById("ham3").style.clipPath = "polygon(0% "+ham3_top_hover+", "+ham3_top_hover+" "+ham3_top_hover+", 100% "+ham3_top_hover+", 100% 100%, "+ham1_btm_hover+" 100%, 0% 100%)";
+            
+        } else {
+            document.getElementById("ham1").style.clipPath = "polygon(0% "+ham1_btm+",0% 0%, "+ham1_btm+" 0%, 100% 0%, 100% "+ham1_btm+", 100% "+ham1_btm+")";
+            document.getElementById("ham2").style.clipPath = "polygon(0% "+ham2_top+", 100% "+ham2_top+", 100% "+ham2_btm+", 0% "+ham2_btm+")";
+            document.getElementById("ham3").style.clipPath = "polygon(0% "+ham3_top+", "+ham3_top+" "+ham3_top+", 100% "+ham3_top+", 100% 100%, "+ham1_btm+" 100%, 0% 100%)";
+        }
+    } else {
+        if (action == 'onmouseover') {
+            document.getElementById("ham1").style.clipPath = "polygon(0% "+x_top_hover+", 0% 0%, "+x_top_hover+" 0%, 100% "+x_btm_hover+", 100% 100%, "+x_btm_hover+" 100%)";
+            document.getElementById("ham2").style.clipPath = "polygon(50% "+ham2_top_hover+", 50% "+ham2_top_hover+", 50% "+ham2_btm_hover+", 50% "+ham2_btm_hover+")";
+            document.getElementById("ham3").style.clipPath = "polygon(0% "+x_btm_hover+", "+x_btm_hover+" 0%, 100% 0%, 100% "+x_top_hover+", "+x_top_hover+" 100%, 0% 100%)";
+        } else {
+            document.getElementById("ham1").style.clipPath = "polygon(0% "+x_top+", 0% 0%, "+x_top+" 0%, 100% "+x_btm+", 100% 100%, "+x_btm+" 100%)";
+            document.getElementById("ham2").style.clipPath = "polygon(50% "+ham2_top+", 50% "+ham2_top+", 50% "+ham2_btm+", 50% "+ham2_btm+")";
+            document.getElementById("ham3").style.clipPath = "polygon(0% "+x_btm+", "+x_btm+" 0%, 100% 0%, 100% "+x_top+", "+x_top+" 100%, 0% 100%)";
+        }
+    }
+
+    if (action == "onmouseover") {
+        document.getElementById("ham1").style.backgroundColor = light_color_grey_1;
+        document.getElementById("ham2").style.backgroundColor = light_color_grey_1;
+        document.getElementById("ham3").style.backgroundColor = light_color_grey_1;
+    } else {
+        document.getElementById("ham1").style.backgroundColor = light_color_grey_2;
+        document.getElementById("ham2").style.backgroundColor = light_color_grey_2;
+        document.getElementById("ham3").style.backgroundColor = light_color_grey_2;
+    }
+
+}
+
+
+window.onresize = function(event) {
+    if (screen.width >= 600 && displayed != "ham") {
+        manageHam();
+    }
+};
+
+
+
+// const email_address = "devin.sze@berkeley.edu";
+
+function askEmail() {
+    document.getElementById("mail_span").style.display = "block";
+    document.getElementById("mail_arrow").style.display = "block";
+}
+// function copyEmail() {
+//     var copyText = document.getElementById("email_copy");
+//     copyText.select();
+//     copyText.setSelectionRange(0, 99999);
+//     navigator.clipboard.writeText(copyText.value);
+
+
+
+
+
+//     // navigator.clipboard.writeText("devin.sze@berkeley.edu");
+//     document.getElementById("mail_span").innerHTML = "Copied: devin.sze@berkeley.edu";
+//     // console.log(document.getElementById("mail_span"));
+// }
+
+
+
+
+function resetEmail() {
+    document.getElementById("mail_span").style.display = "none";
+    document.getElementById("mail_arrow").style.display = "none";
+    // document.getElementById("mail_span").innerHTML = "Copy to Clipboard";
+}
+
+var mail_span = document.getElementById("mail_span");
+window.onscroll = function() {
+    if (mail_span.style.display != "none" && mail_span.getBoundingClientRect().bottom <= 0) {
+        resetEmail(); //console.log("REACHED");
+    }
+}
+
+
+function openNewTab(link) {
+    window.open(link);
+}
+
+
+function closeImage() {
+    document.getElementById("largeImage").style.display = "none";
+    document.getElementById("image_container").style.display = "none";
+}
+
+function openImage(img) {
+    document.getElementById("largeImage").style.backgroundImage = "url(" + img.src + ")";
+    document.getElementById("largeImage").style.display = "block";
+    document.getElementById("image_container").style.display = "flex";
+    
+    document.getElementById("largeImage").style.opacity = "1";
+    document.getElementById("image_container").style.opacity = "1";
+}
+
+
+
+// function toTop
+function scrollToTop() {
+    window.scrollTo(0, 0);
+}
+function manage_to_top(action) {
+    console.log(action);
+    if (action == 'onmouseenter') {
+        document.getElementById("to_top").style.transform = "translateY(-4px)";
+        document.getElementById("to_top").style.boxShadow = shadow_hover;
+    } else {
+        document.getElementById("to_top").style.transform = "translateY(0px)";
+        document.getElementById("to_top").style.boxShadow = "";
+    }
+}
+
+
+
+
+
+/**
+ * https://css-tricks.com/styling-based-on-scroll-position/
+ */
+
+// The debounce function receives our function as a parameter
+const debounce = (fn) => {
+
+    // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+    let frame;
+  
+    // The debounce function returns a new function that can receive a variable number of arguments
+    return (...params) => {
+      
+      // If the frame variable has been defined, clear it now, and queue for next frame
+      if (frame) { 
+        cancelAnimationFrame(frame);
+      }
+  
+      // Queue our function call for the next frame
+      frame = requestAnimationFrame(() => {
+        
+        // Call our function and pass any params we received
+        fn(...params);
+      });
+  
+    } 
+  };
+  
+  
+  // Reads out the scroll position and stores it in the data attribute
+  // so we can use it in our stylesheets
+  const storeScroll = () => {
+    document.documentElement.dataset.scroll = window.scrollY;
+  }
+  
+  // Listen for new scroll events, here we debounce our `storeScroll` function
+  document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+  
+  // Update scroll position for first time
+  storeScroll();
---
+++ metaball.js
@@ -0,0 +1,216 @@
+
+const INF = Number.POSITIVE_INFINITY;
+
+
+var container_x_bound_left = parseFloat(document.getElementById("ball_container").getBoundingClientRect().left); //window.innerWidth;window.innerWidth; //1050;
+var container_x_bound_right = parseFloat(document.getElementById("ball_container").getBoundingClientRect().right);
+var container_x_bound_top = parseFloat(document.getElementById("ball_container").getBoundingClientRect().top); //window.innerWidth;window.innerWidth; //1050;
+var container_y_bound_bottom = parseFloat(document.getElementById("ball_container").getBoundingClientRect().bottom); //window.innerHeight;
+
+var container_x_bound = container_x_bound_right - container_x_bound_left;
+var container_y_bound = container_y_bound_bottom - container_x_bound_top;
+
+var padding = 25;
+
+var x_bound_l = padding;
+var x_bound_r = container_x_bound - padding;
+var y_bound_t = padding;
+var y_bound_b = container_y_bound - padding;
+
+var created = false;
+var shown = true; // if the text is shown
+var moving = false;
+var play_pause_on = false; // if mouse is on the play/pause button
+
+var ball_widths = [65, 70, 96, 100, 108, 117, 147, 200, 250]
+var ball_dx = [18, 16, -37/2, 30/2, -55/2, 46/2, 60/2, -30/2, 5/2, -8/2]
+var ball_dy = [-2, 12, 10/2, -40/2, -20/2, 25/2, -13/2, 24/2, 15/2, -3/2]
+// var ball_left = [-1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000]
+// var ball_top = [-1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000]
+
+
+var gridDisplay = document.getElementById("ball_container");
+
+
+
+window.addEventListener('resize', function(event) {
+    // container_x_bound = parseFloat(document.getElementById("ball_container").getBoundingClientRect().right); //window.innerWidth;
+    // container_y_bound = parseFloat(document.getElementById("ball_container").getBoundingClientRect().bottom); //window.innerHeight;
+    // x_bound_r = container_x_bound; // - padding;
+    // y_bound_b = container_y_bound; // - padding;
+
+
+    container_x_bound_left = parseFloat(document.getElementById("ball_container").getBoundingClientRect().left); //window.innerWidth;window.innerWidth; //1050;
+    container_x_bound_right = parseFloat(document.getElementById("ball_container").getBoundingClientRect().right);
+    container_x_bound_top = parseFloat(document.getElementById("ball_container").getBoundingClientRect().top); //window.innerWidth;window.innerWidth; //1050;
+    container_y_bound_bottom = parseFloat(document.getElementById("ball_container").getBoundingClientRect().bottom); //window.innerHeight;
+    
+    container_x_bound = container_x_bound_right - container_x_bound_left;
+    container_y_bound = container_y_bound_bottom - container_x_bound_top;
+    
+    
+    x_bound_r = container_x_bound - padding;
+    y_bound_b = container_y_bound - padding;
+
+
+}, true);
+
+
+
+
+function create_balls() {
+    // return;
+    if (created) {
+        return;
+    }
+
+    for (let i=0; i < ball_widths.length; i++) {
+        ball = document.createElement('div')
+        ball.id = "b" + String(i);
+        ball.className = "balls";
+
+        ball.style.width = ball_widths[i] + "px";
+        ball.style.height = ball_widths[i] + "px";
+        ball.style.left = "calc(50% - " + ball_widths[i]/2 + "px"; //ball_left[i] + "px";
+        ball.style.top = "calc(50% - " + ball_widths[i]/2 + "px"; //ball_top[i] + "px";
+
+        gridDisplay.appendChild(ball);
+
+        // jitter dx dy
+        ball_dx[i] = Math.round((ball_dx[i]*0.95 - ball_dx[i]/10 * Math.random()) * 10) / 10;
+        ball_dy[i] = Math.round((ball_dy[i]*0.95 - ball_dy[i]/10 * Math.random()) * 10) / 10;
+    }
+    created = true;
+    // return;
+    setTimeout(() => {
+        // after delay, start moving the balls
+        start_stop_balls();
+    }, 400)
+}
+
+// var xx =285
+
+function move_balls() {
+    // move all balls
+    // console.log(xx);
+    // document.getElementById("ball_container").style = "filter: blur(15px) contrast(10000) opacity(50%) hue-rotate(" + xx + "deg);";
+    // xx+=1;
+    
+
+
+    for (i=0; i<ball_widths.length; i++) {
+        move_ball(i);
+    }
+}
+
+
+function move_ball(i) {
+    var b_id = "b" + String(i)
+
+    // get bounds
+    var container_bounds = document.getElementById("ball_container").getBoundingClientRect();
+    var ball = document.getElementById(b_id);
+    var ball_bound = ball.getBoundingClientRect();
+
+    // calculate bounds    
+    var container_l = parseFloat(container_bounds.left);
+    var container_t = parseFloat(container_bounds.top);
+
+    // calculate current and next ball positions
+    // these are all relative, padding included in positions
+    var curr_l = parseFloat(ball_bound.left) - container_l;
+    var next_l = curr_l + ball_dx[i];
+    
+    var curr_r = parseFloat(ball_bound.right) - container_l;
+    var next_r = curr_r + ball_dx[i];
+
+    var curr_t = parseFloat(ball_bound.top) - container_t;
+    var next_t = curr_t + ball_dy[i];
+
+    var curr_b = parseFloat(ball_bound.bottom) - container_t;
+    var next_b = curr_b + ball_dy[i];
+
+    // calculate LEFT position
+    if (x_bound_r <= next_r) {
+        ball.style.left = x_bound_r - ball_widths[i] + "px";
+        ball_dx[i] = -1 * ball_dx[i];
+    } else if (next_l <= x_bound_l) {
+        ball.style.left = x_bound_l + "px";
+        ball_dx[i] = -1 * ball_dx[i];
+    } else {
+        ball.style.left = next_l + "px"
+    }
+
+    // calculate TOP position
+    if (y_bound_b <= next_b) {
+        ball.style.top = y_bound_b - ball_widths[i] + "px";
+        ball_dy[i] = -1 * ball_dy[i];
+    } else if (next_t <= y_bound_t) {
+        ball.style.top = y_bound_t + "px";
+        ball_dy[i] = -1 * ball_dy[i];
+    } else {
+        ball.style.top = next_t + "px"
+    }
+}
+
+
+
+function start_stop_balls() {
+    if (moving) {
+        // if currently moving, stop movement
+        moving = !moving;
+        document.getElementById("play_pause").title = "play simulation";
+        document.getElementById("pp1").style.clipPath = "polygon(0% 0%, 100% 25%, 100% 75%, 0% 100%)";
+        document.getElementById("pp2").style.clipPath = "polygon(0% 25%, 100% 50%, 100% 50%, 0% 75%)";
+    } else {
+        // if not currently moving, start movement
+        moving = !moving;
+        document.getElementById("play_pause").title = "pause simulation";
+        document.getElementById("pp1").style.clipPath = "polygon(0% 10%, 50% 10%, 50% 90%, 0% 90%)";
+        document.getElementById("pp2").style.clipPath = "polygon(50% 10%, 100% 10%, 100% 90%, 50% 90%)";
+        continuous_balls();
+    }
+}
+
+
+// https://stackoverflow.com/questions/5226285/settimeout-in-for-loop-does-not-print-consecutive-values
+
+// continouosly move the balls (unless `moving` global variable is set to false) 
+function continuous_balls() {
+    var iters = INF;
+    var delay = 400;
+
+    (function myLoop(i) {
+        setTimeout(function() {
+            if (!moving) return;
+            move_balls();
+            if (--i) myLoop(i);
+        }, delay)
+    })(iters);
+}
+
+
+var DARK_MODE = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+var light_transparent_color_3 = "#afafafc0"; //808080";
+var light_color_grey_1 = "#808080";
+var light_color_line = "#dddddd";
+
+var hover_color = light_color_grey_1;
+var original_color = light_transparent_color_3;
+if (DARK_MODE) {
+    hover_color = light_color_line;
+    original_color = light_transparent_color_3;
+}
+
+
+function play_pause_mouse() {
+    console.log("!!!");
+    if (play_pause_on) {
+        document.getElementById("pp1").style.backgroundColor = original_color; 
+        document.getElementById("pp2").style.backgroundColor = original_color;
+    } else {
+        document.getElementById("pp1").style.backgroundColor = hover_color;
+        document.getElementById("pp2").style.backgroundColor = hover_color;
+    }
+    play_pause_on = !play_pause_on;
+}
